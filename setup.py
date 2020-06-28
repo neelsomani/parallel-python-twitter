e8114be266d756a84b695d88a59abf89ffa02612
Neel Somani <neel@berkeley.edu>
2020-06-28 03:05:01 -0500

    Update PyPI version

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='parallel-python-twitter',
     packages=['parallel_twitter'],
-    version='0.1.0',
+    version='0.2.0',
     license='MIT',
     description='Python Twitter client to distribute requests across API keys',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Neel Somani',
     author_email='neeljaysomani@gmail.com',
     url='https://github.com/neelsomani/parallel-python-twitter',
